719f89612d95a576d59489548291d974cf7547b5
Rhea <rheaazarraga@gmail.com>
2022-03-04 20:36:13 -0800

    set global variables for possible selections and which selector beats which

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 const selectionButtons = document.querySelectorAll("[data-selection]");
+const SELECTIONS = [
+  {
+    name: "rock",
+    emoji: "🪨",
+    beats: "scissors"
+  },
+  {
+    name: "paper",
+    emoji: "📃",
+    beats: "rock"
+  },
+  {
+    name: "scissors",
+    emoji: "✂️",
+    beats: "paper"
+  },
+
+]
 
 selectionButtons.forEach(selectionButton => {
   selectionButton.addEventListener("click", e => {
